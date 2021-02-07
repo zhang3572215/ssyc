ae68dd7257e151b80e830a3c1c30ba00f964f06c
zhang3572215 <zhangchuang_168@163.com>
2021-02-07 14:18:22 +0800

    新增搜索页面，完善主业内容

--- pages/index/search/search.vue
+++ pages/index/search/search.vue
@@ -1,22 +1,100 @@
 <template>
 	<veiw class="search">
-		<uni-nav-bar :fixed="false" color="#333333" background-color="#FFFFFF" right-icon="scan" @clickLeft="showCity" @clickRight="scan">
-			<block slot="left">
-				<view class="city">
-					<view><text class="uni-nav-bar-text">{{ city }}</text></view>
-					<uni-icons type="arrowdown" color="#333333" size="22" />
+		<view class="container">
+			<uni-search-bar :radius="100" @confirm="search"cancelButton="auto"></uni-search-bar>
+			<view class="history">
+				<view class="history-title">搜索历史</view>
+				<view class="history-list">
+					<view class="history-item" v-for="(item,index) in listArr" :key="index">{{item}}</view>
 				</view>
-			</block>
-			<view class="input-view">
-				<uni-icons class="input-uni-icon" type="search" size="22" color="#666666" />
-				<input confirm-type="search" class="nav-bar-input" type="text" placeholder="输入搜索关键词" @confirm="confirm">
 			</view>
-		</uni-nav-bar>
+		</view>		
 	</veiw>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				listArr:['苹果','菠萝','水果','随便搜搜']
+			}
+		},
+		mounted() {
+			let that = this;
+			uni.getStorage({
+				key: 'SEARCH_RESULT',
+				success: res => {
+					console.log(res.data)
+					if (res.data) {
+						let str = JSON.parse(res.data)
+						console.log(str);
+						that.listArr = str
+					}
+				}
+			})
+		},
+		methods: {
+			search(e){
+				let that = this
+				console.log(e.value)
+				if (that.listArr.length > 7) {
+					that.listArr.pop()
+					that.listArr.unshift(e.value)
+					uni.setStorage({
+						key: 'SEARCH_RESULT',
+						data: JSON.stringify(that.listArr),
+						success: res => {
+							uni.navigateTo({
+								url: '../../hall/hall'
+							})
+						}
+					})
+				}else {
+					that.listArr.push(e.value)
+					uni.setStorage({
+						key: 'SEARCH_RESULT',
+						data: JSON.stringify(that.listArr),
+						success: res => {
+							uni.navigateTo({
+								url: '../../hall/hall'
+							})
+						}
+					})
+				}
+			}
+		}
+	}
 </script>
 
 <style>
+	.container {
+		background-color: #F7F7F7;
+	}
+	.history {
+		margin-top: 10rpx;
+		padding: 0 30rpx;
+		background-color: #fff;
+	}
+	.history-title {
+		padding: 30rpx 0;		
+		font-size: 28rpx;
+		font-weight: normal;
+		color: #333333;
+		border-bottom: 2rpx solid #ddd;
+	}
+	.history-list {
+		padding: 30rpx 0;
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.history-item {
+		width: 50%;
+		padding-bottom: 30rpx;
+		font-size: 24rpx;
+		font-weight: normal;
+		color: #333333;
+	}
 </style>
